2915153c4fb9de1869d8e7960da89295a90625ab
Lipezxl7 <felipechoji28@gmail.com>
2026-01-16 19:39:36 -0300

    Bot 24h configurado

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 5000);
+
 const crypto = require('crypto')
 if (!global.crypto) {
   global.crypto = crypto
